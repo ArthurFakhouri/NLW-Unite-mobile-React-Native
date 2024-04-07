2f2b0be971bb5bf169947d8166c42aa139cd7df8
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-04-07 14:10:57 -0300

    feat: Adding tailwindCSS in our application and setting up app.json when loading on initial moment of our application

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function Home() {
+    return (
+        <View className='flex-1 bg-green-500 items-center justify-center'>
+            <Text className='text-white'>Hello World</Text>
+        </View>
+    )
+}
