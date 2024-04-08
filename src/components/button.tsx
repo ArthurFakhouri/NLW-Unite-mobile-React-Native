cf1bf6e6729727a362d1824b2c817d5e9d53068f
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-04-07 21:36:00 -0300

    style and refactor - applying a different text color and setting up props to be first one to be inserted on component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -10,15 +10,15 @@ export function Button({ title, isLoading = false, ...rest }: ButtonProps) {
 
     return (
         <TouchableOpacity
+            {...rest}
             activeOpacity={0.7}
             disabled={isLoading}
             style={{ backgroundColor: "#F48F56", width: '100%', height: 56, justifyContent: 'center', alignItems: 'center', borderRadius: 8 }}
-            {...rest}
         >
             {isLoading ? (
                 <ActivityIndicator className="text-green-500" />
             ) : (
-                <Text className=" text-base font-bold uppercase">
+                <Text className="text-green-500 text-base font-bold uppercase">
                     {title}
                 </Text>
             )}
