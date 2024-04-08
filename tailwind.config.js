2bde55456d3e1ec300f3f686339b8ccaf197d5c5
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-04-07 21:35:20 -0300

    refactor - adjusting tailwind and typescript config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-import { colors } from './src/styles/colors'
-import { fontFamily } from './src/styles/fontFamily'
+import { colors } from "./src/styles/colors"
+import { fontFamily } from "./src/styles/fontFamily"
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    // NOTE: Update this to include the paths to all of your component files.
     content: ["./src/**/*.{ts,tsx}"],
     presets: [require("nativewind/preset")],
     theme: {
